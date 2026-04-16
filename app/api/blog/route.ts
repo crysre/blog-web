d60d91f67fed9c96f6e546030d70d246c3901d15
Tamojit Hazra <tamosrk@gmail.com>
2026-04-17 04:06:07 +0530

    fixed routes

--- app/api/blog/route.ts
+++ app/api/blog/route.ts
@@ -1,55 +1,107 @@
+import { authOptions } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
+import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest) {
-  const data = await req.json();
 
-  const blogsData = await prisma.blog.findUnique({
-    where: { id: data.id },
+
+
+export async function GET(req: NextRequest) {
+  const session = await getServerSession(authOptions);
+
+  if(!session){
+  return NextResponse.json({error: "Unauthorized"}, {status:401});
+}
+
+
+  const blogsData = await prisma.blog.findMany({
+    //@ts-ignore
+    where: { authorId: session.user.id },
+    orderBy:{createdAt:"desc",}
   });
   return NextResponse.json({
-    message: blogsData,
+    blogsData,
   });
 }
 
+
+
+
 export async function POST(req: NextRequest) {
-  const id = 1;
+  const session = await getServerSession(authOptions)
+
+if(!session){
+  return NextResponse.json({error: "Unauthorized"}, {status:401});
+}
+
   const data = await req.json();
 
-  await prisma.blog.create({
+  const blog = await prisma.blog.create({
     data: {
       title: data.title,
       content: data.content,
       published: data.published,
-      authorId: data.id,
+      // @ts-ignore
+      authorId: session.user.id,
     },
   });
   return NextResponse.json({
     message: "you've been signed up",
+    blog
   });
 }
 
 export async function PUT(req: NextRequest) {
+  const session = await getServerSession(authOptions)
+
+if(!session){
+  return NextResponse.json({error: "Unauthorized"}, {status:401});
+}
+
   const data = await req.json();
 
-  await prisma.blog.update({
+  const blog = await prisma.blog.findUnique({
+    where: {id: data.id},
+  })
+//@ts-ignore
+  if(!blog || blog?.authorId != session.user.id ){
+    return NextResponse.json({error: "Forbidden"}, {status:403});
+  }
+
+  const updated = await prisma.blog.update({
     where: { id: data.id },
     data: {
       title: data.title,
       content: data.content,
-      published: data.published,
-      authorId: data.id,
-    },
+      published: data.published   },
   });
   return NextResponse.json({
-    message: "blog has been deleted",
+    message: "blog has been updated",
+    updated
   });
 }
 
 export async function DELETE(req: NextRequest) {
+  const session = await getServerSession(authOptions)
+
+if(!session){
+  return NextResponse.json({error: "Unauthorized"}, {status:401});
+}
+
+
   const data = await req.json();
 
+  const blog = await prisma.blog.findUnique({
+    where: {id: data.id},
+  })
+//@ts-ignore
+  if(!blog || blog?.authorId != session.user.id ){
+    return NextResponse.json({error: "Forbidden"}, {status:403});
+  }
+
   await prisma.blog.delete({
-    where: { id: data.id, email: data.email },
+    where: { id: data.id },
   });
+
+  return NextResponse.json({message: "Deleted"});
 }
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,63 +1,18 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { NextAuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 import { prisma } from "./prisma";
 
 
 export const authOptions: NextAuthOptions = {
-  // pages:{
-  //   signIn:"/signin",
-  //   error:"/signin",
-  // },
+  
   session: {
     strategy: "jwt",
   },
   adapter: PrismaAdapter(prisma)
   ,
   providers: [
-//     CredentialsProvider({
-//       name: "credentials",
-//       credentials: {
-//         email: {
-//           label: "Email",
-//           type: "email",
-//           placeholder: "Hello@gmail.com",
-//         },
-//         password: {
-//           label: "Password",
-//           type: "password",
-          
-//         },
-//         name:{
-//           label:"Name",
-//           type:"text"
-//         }
-//       },
-//       async authorize(credentials) {
-//   if (!credentials) return null;
 
-//   let user = await prisma.user.findUnique({
-//     where: { email: credentials.email as string },
-//   });
-
-//   if (!user) {
-//     user = await prisma.user.create({
-//       data: {
-//         email: credentials.email as string,
-//         name: credentials.name as string,
-//         // password: hash(credentials.password)
-//       },
-//     });
-//   }
-
-//   return {
-//     id: user.id.toString(),
-//     name: user.name,
-//     email: user.email,
-//   };
-// },
-//     }),
     GoogleProvider({
     clientId: process.env.GOOGLE_CLIENT_ID as string,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
@@ -86,6 +41,7 @@ export const authOptions: NextAuthOptions = {
 
     session.user.email = token?.email ?? null;
     session.user.name = token?.name ?? null;
+    session.user.id = token?.id;
 
     return session;
   },
