a7dbe31df83d5e11f416de3dff959827745bae76
Tamojit Hazra <tamosrk@gmail.com>
2026-04-17 00:34:20 +0530

    oauth configured

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,33 +1,63 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
+import { prisma } from "./prisma";
 
 
 export const authOptions: NextAuthOptions = {
+  // pages:{
+  //   signIn:"/signin",
+  //   error:"/signin",
+  // },
+  session: {
+    strategy: "jwt",
+  },
+  adapter: PrismaAdapter(prisma)
+  ,
   providers: [
-    CredentialsProvider({
-      name: "credentials",
-      credentials: {
-        email: {
-          label: "Email",
-          type: "email",
-          placeholder: "Hello@gmail.com",
-        },
-        password: {
-          label: "Password",
-          type: "password",
+//     CredentialsProvider({
+//       name: "credentials",
+//       credentials: {
+//         email: {
+//           label: "Email",
+//           type: "email",
+//           placeholder: "Hello@gmail.com",
+//         },
+//         password: {
+//           label: "Password",
+//           type: "password",
           
-        },
-      },
-      async authorize(credentials) {
-        console.log(credentials);
-        return {
-          id: "1",
-          name: "Tanuj",
-          email: "tamo@gmail.com",
-        };
-      },
-    }),
+//         },
+//         name:{
+//           label:"Name",
+//           type:"text"
+//         }
+//       },
+//       async authorize(credentials) {
+//   if (!credentials) return null;
+
+//   let user = await prisma.user.findUnique({
+//     where: { email: credentials.email as string },
+//   });
+
+//   if (!user) {
+//     user = await prisma.user.create({
+//       data: {
+//         email: credentials.email as string,
+//         name: credentials.name as string,
+//         // password: hash(credentials.password)
+//       },
+//     });
+//   }
+
+//   return {
+//     id: user.id.toString(),
+//     name: user.name,
+//     email: user.email,
+//   };
+// },
+//     }),
     GoogleProvider({
     clientId: process.env.GOOGLE_CLIENT_ID as string,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
@@ -40,27 +70,25 @@ export const authOptions: NextAuthOptions = {
     }
   }),
   ],
-  session: {
-    strategy: "jwt",
-  },
   secret: process.env.NEXTAUTH_SECRET,
-  callbacks:{
-    async jwt({token, user}){
-      if(user){
-        token.id =  user.id
-      }
-      return token
-    },
-    async session({session, token}){
-      if(session.user){
-        //@ts-ignore
-        session.user.id = token.id
-      }
-      return session
-    },
-    
+  callbacks: {
+  async jwt({ token, user }: any) {
+    if (user) {
+      token.id = user.id;
+      token.email = user.email;
+      token.name = user.name;
+    }
+    return token;
   },
-  pages: {
-    signIn: "/signin"
-  }
+
+  async session({ session, token }: any) {
+    if (!session.user) session.user = {};
+
+    session.user.email = token?.email ?? null;
+    session.user.name = token?.name ?? null;
+
+    return session;
+  },
+}
+
 };
